9da674a22e12351558efa8bc7bc62c662f4e8929
Marcos Gobbo <marcoszgobbo@gmail.com>
2025-01-14 11:32:23 -0300

    Starting Project

---
+++ src/main/java/com/gobbo/java_chat_app/JavaChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.gobbo.java_chat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChatAppApplication.class, args);
+	}
+
+}
